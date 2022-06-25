6bb48c2a7c8e5c534cf8e518df8f9ae0757b92bb
tanuchicken <tarosakane1113@gmail.com>
2022-06-26 01:04:56 +0900

    nuxt setup

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app>
+        <v-app-bar dence color="blue">
+            <v-app-bar-nav-icon
+            @click="drawer=!drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>this is header</v-toolbar-title>
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer" fixed>
+
+        </v-navigation-drawer>
+
+        <div>
+            <nuxtPage />
+        </div>
+    </v-app>
+</template>
+
+<script>
+export default {
+    setup() {
+        let drawer = ref(true)
+
+        return {
+            drawer
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+<h1>
+    this is top page.
+</h1>
+</template>
